bb991cae4f7163021e97d8fad98a0a32c08586ea
Eagle57 <94141086+Eagle57f@users.noreply.github.com>
2024-12-04 14:16:00 +0100

    Add things to TP1

--- TP1/TP1.py
+++ TP1/TP1.py
@@ -1,5 +1,5 @@
 # Ex 1
-print(f"""
+'''print(f"""
 125*347={125*347}
 4x3,52+(2,5-7,5)^3={4*3.52+(2.5-7.5)**3}
 (3,25+7,8)/(1,26-0,57)={(3.25+7.8)/(1.26-0.57)}
@@ -11,29 +11,44 @@ a=125, b=11 : {divmod(125,11)} OU a=125, b=11 : {(125//11,125%11)}
 a=689, b=17 : {divmod(689,17)} OU a=689, b=17 : {(689//17,689%17)}
 a=-145, b=21 : {divmod(-145,21)} OU a=-145, b=21 : {(-145//21,-145%21)}
 a=-368, b=-6 : {divmod(-368,-6)} OU a=-368, b=-6 : {(-368//-6,-368%-6)}
-""")
+""")'''
 
 # Ex 2
-print("Erreur, 1, 1, 0")
+# a) Erreur, b) 1, c) 1, d) 0
 
 #Ex 3
+"""
+# a)
 a=5
 b=2
 a,b=b,a
 print(a,b)
 
-# Voir sujet
+# b) x=x+5 revient à incrémenter x de 5"""
 
 # Ex 4
-# Voir sujet
+"""# a)
+a,b,c,d,e=0,2,4,6,8 # a=0, b=2, c=4, d=6 e=8
+a,b,c,d,e=a,d,c,b,a # a=a=0, b=d=6, c=c=4, d=b=2, e=a=0
+print(a,b,c,d,e)
 
+# b)
 a,b,c=2,3,-5
 b=2
 c=3*a
-print(a,b,c)
+print(a,b,c)"""
 
 # Ex 5
-# Voir sujet
+"""# Pour les nombres négatifs, int enlève la partie décimale (donc au supérieur près pour les négatifs)
+# alors que floor arrondit à l'inférieur près (pour les positifs et négatifs).
+
+from fractions import *
+a=Fraction(3,5)
+print(a) # Print la fraction simplifiée au maximum
+p=a.numerator
+print(p) # Print le numérateur
+q=a.denominator
+print(q) # Print le dénominateur 
 
 from fractions import Fraction
 print((1+Fraction(3,4))/(Fraction(5,2)+Fraction(7,3)))
@@ -42,7 +57,14 @@ print(Fraction((1+Fraction(3,4)),(Fraction(5,2)+Fraction(7,3))))
 
 print((Fraction(19,15)-Fraction(3,7))/(Fraction(11,2)+Fraction(8,35)))
 # OU
-print(Fraction((Fraction(19,15)-Fraction(3,7)),(Fraction(11,2)+Fraction(8,35))))
+print(Fraction((Fraction(19,15)-Fraction(3,7)),(Fraction(11,2)+Fraction(8,35))))"""
 
 # Ex 6
-# Voir sujet
+"""from math import *
+a=sqrt(2) # Racine carré de 2
+b=sqrt(3) # Racine carré de 3
+c=5
+print(a**2==2) # False car il y a une erreur d'arrondi
+print((sqrt(3))**2==3) # False car il y a une erreur d'arrondi
+print(a**2+b==c) # True car les erreurs d'arrondi se compensent
+print(a> 1.732) # False car inférieur"""
