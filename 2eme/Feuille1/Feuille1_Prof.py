97e4f6f8f2e8dde211fac3ae882424a3b4edbd53
Eagle57 <94141086+Eagle57f@users.noreply.github.com>
2025-10-16 08:41:00 +0200

    Deployed 6444a99 with MkDocs version: 1.6.1

--- 2eme/Feuille1/Feuille1_Prof.py
+++ 2eme/Feuille1/Feuille1_Prof.py
@@ -4,7 +4,6 @@
 # ===========================================================
 
 
-
 import numpy as np  # pour calcul matriciel
 import matplotlib.pyplot as plt  #ma biblio pour la représenta graphique en 2D
 from math import*
@@ -36,7 +35,7 @@ for k in range(n+1):
 def inverse1(L):
     M=[1/k for k in L if k!=0]
     return M
-    
+
 #version 2
 
 def inverse2(L):
@@ -72,7 +71,7 @@ listecoeffs=eval(input('Donner la liste des coefs:'))
 S=0
 for k in range (len(notes)):
     S=S+notes[k]*listecoeffs[k]
-    M=S/sum(listecoeffs, notes)
+    M=S/sum(listecoeffs)
 print("La moyenne est", M)
 
 
@@ -81,13 +80,13 @@ print("La moyenne est", M)
 def echange(L,i,j):
     if 0<=i<=len(L)-1 and 0<=j<=len(L)-1:
         #les indices vont de 0 à longueur -1
-        L[i], L[j]=L[j]=L[i]
+        L[i], L[j]=L[j],L[i]
     else:
         L='changer i ou j'
     return L
-    
-    
-    
+
+
+
 def echange2(L,i,j):
     if 0 <=i<=len(L)-1 and 0<=j<=len(L)-1:
         X=L[i]
@@ -96,4 +95,208 @@ def echange2(L,i,j):
     else:
         L="changer l'un des argument-s i ou j"
     return L
-    
+
+##5)
+
+def supp1(L,x):
+    L1=[y for y in L if y!=x]
+    return L1
+
+# ou
+
+def supp2(L,x):
+    L2=[]
+    for k in range (0,len(L)):
+        if L[k]!=x:
+            L2.append(L[k])
+    return L2
+
+def supp3(L,x):
+    L3=[]
+    for l in L:
+        if l!=x:
+            L3=L3+[l]
+    return L3
+
+##6
+
+def insertion(L,i,x):
+    if 0<=i<=len(L)-1:
+
+       L1=L[0:i]   #extraction d'une liste de 0 à i-1
+       L2=L[i:len(L)]   #extraction d'une liste de i à longueur -1
+       L=L1+[x]+L2
+    else:
+        L="l'indice i n'appartient à l'intervalle"
+    return L
+
+def insertion2(L,i,x):
+    if 0<=i<=len(L)-1:
+        L.insert(i,x)
+    else:
+        L="l'indice i n'appartient à l'intervalle"
+    return L
+
+##7
+def mesmax1(L):
+    maxi=max(L)
+    return maxi, L.count(maxi)
+
+def mesmax2(L):
+    maxi=L[0] #on suppose que le 1er terme est le max
+    c=0
+    for x in L:
+        if x>maxi:
+            maxi=x
+            c=1
+        elif x==maxi:
+            c=c+1
+    return maxi, c
+
+
+##8
+
+def supdouble(L):
+    NL=[]
+    for x in L:
+        if x not in NL:
+            NL=NL+[x]
+    return NL
+
+##9
+
+def appro(L):
+    ecart=abs(L[0]-L[1])
+    a,b=L[0], L[1]
+    for i in range(0,len(L)):
+        for j in range (i+1, len(L)):
+            if abs(L[i]-L[j]) <ecart:
+                ecart=abs(L[i]-L[j])
+                a,b=L[i], L[j]
+    return a,b
+
+
+##10
+
+n=int(input("n?"))
+L=[i+j for i in range(0,n) for j in range (i+1, n+1)]
+print('sommeV1', sum(L))
+
+
+n=int(input("n?"))
+S=0
+for i in range (0,n):
+    for j in range(i+1,n+1):
+        S=S+i+j
+print('sommev2=',S)
+
+def sommedouble(n):
+    s=0
+    for i in range (0,n):
+        for j in range (i+1,n+1):
+            s=s+i+j
+    return 'sommev3=',s
+
+
+
+#exercice 1
+
+
+def factorielle(n):
+    P=1
+    for k in range(1,n+1):
+        P=P*k
+    return P
+x=range(0,50) #axe des abcisse
+u=[factorielle(n)/n**n for n in x]
+plt.plot(x,u, 'xg')
+plt.show()
+
+
+n=int(input('N?'))
+P=1
+u=[]
+for k in range(1,n+1):
+    P=P*k
+    u.append(P/k**k)
+x=range(0,n) #axe des abcisses
+plt.plot(x,u,'*r')
+plt.show()
+
+
+ #exercice 2
+
+def f(n,x):
+    y=x**n+x-1
+    return y
+
+def dicho(n):
+    a,b=0,1
+    while abs(b-a)>10**(-3):
+        m=(a+b)/2
+        if f(n,m)==0:
+            a,b= m,m
+        elif f(n,a)*f(n,m)<0:
+            b=m
+        elif f(n,a)*f(n,m)>0:
+            a=m
+    return (a+b)/2
+
+
+n=int(input('n?'))
+x=np.linspace(0,1,501)
+for k in range (1,n+1):
+    y= f(k,x)
+    plt.plot(x,y)
+    plt.plot(dicho(k),0,'rx')
+plt.grid()
+plt.show()
+
+
+#exercice 3
+
+#version 1
+
+def suiteuV1(n):
+    u,v=0,1
+    for k in range (2,n+1):
+        w=u+v  #un+2=un+1+un
+        u,v=v,w # un+1=un et un+1=un+2
+    return v
+
+#version 2
+
+def suiteuV2(n):
+    u,v=0,1
+    for k in range (2,n+1):
+        w=u+v
+        u=v
+        v=w
+    return v
+
+def nb_or():
+    rho=(1+sqrt(5)/2)
+    p=1
+    while abs(suiteuV1(p+1)/suiteuV1(p))-rho>10**-10:
+        p=p+1
+    return p
+
+#exercice 4
+
+def Edhec21():
+    s=0
+    u=1
+    k=0 #compteur
+    while s<=1000:
+        s=s+u
+        u=sqrt(u**2+u)
+        k=k+1
+    return k
+
+def Edhec21b():
+    u=1
+    k=0
+    while u**2-1<=1000:
+        u=sqrt(u**2+u)
+        k=k+1
+    return k
